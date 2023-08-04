c3fed01a3384f7dd9e3c8e7349402ab8ef967a85
YanJi <ysh@simsoft.top>
2023-08-04 22:21:52 +0800

    。

--- config.js
+++ config.js
@@ -1,21 +1,8 @@
 window.Config = {
-
-  // 显示标题
-  SiteName: 'NLR DevTeam 项目状态监控',
-
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only
-  ApiKeys: [
-    'ur1436354-111ee1bc6757a71a047da5da',
-  ],
-
-  // 日志天数
+  SiteName: 'NLR Status · 项目状态监控',
+  ApiKeys: ['ur1436354-111ee1bc6757a71a047da5da',],
   CountDays: 30,
-
-  // 是否显示检测站点的链接
   ShowLink: false,
-
-  // 导航栏菜单
   Navi: [
     {
       text: 'NLRDev 主页',
