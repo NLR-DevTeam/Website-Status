87b258274f75c32e6002db51903e101515beec4d
YanJi <ysh@simsoft.top>
2023-08-04 21:16:19 +0800

    。

--- config.js
+++ config.js
@@ -1,36 +1,29 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'NLR DevTeam 项目状态监控',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1436354-111ee1bc6757a71a047da5da',
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 30,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'NLRDev 主页',
+      url: 'https://www.nlrdev.top/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
-    },
+      text: '加入社群',
+      url: 'https://join.nlrdev.top/'
+    }
   ],
 };
